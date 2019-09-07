123eecb56f195f58149304ca1e7d5492595ecb18
Lee-Roy King <leeroywking@gmail.com>
2019-09-07 14:50:59 -0700

    wired up

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return <footer id="footer">&copy; 2019 Lee-Roy King</footer>;
+  }
+};
+
+export default Footer;
